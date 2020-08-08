b5c8592bbc7f20f00da088c8e50ba3448c9a45fa
O-Jun-S <Jun.Shima256@gmail.com>
2020-08-08 18:40:18 +0900

    rewrite README and delete ignore mode.

--- README.md
+++ README.md
@@ -1,16 +1,45 @@
-# CL_caesar
+# CL-caesar
 Solve caesar crypto by Common Lisp.
 
 # Demo
 
-```bash
-$ clisp
+```common-lisp:REPL
 (load "solver.lisp")
 
-# If you want to ignore some string, use add-ignore function.
-# ignore "I" and "j".
-(add-ignore "Ij")
-
-(solve "abCdefGXyZ" 10)
--> klMnopQHiJ (stdout)
+; If you are going to solve "RXMS{MNO}".
+(solve-all "RXMS{MNO}")
 ```
+returns
+```
+1>> SYNT{NOP}
+2>> TZOU{OPQ}
+3>> UAPV{PQR}
+4>> VBQW{QRS}
+5>> WCRX{RST}
+6>> XDSY{STU}
+7>> YETZ{TUV}
+8>> ZFUA{UVW}
+9>> AGVB{VWX}
+10>> BHWC{WXY}
+11>> CIXD{XYZ}
+12>> DJYE{YZA}
+13>> EKZF{ZAB}
+14>> FLAG{ABC}
+15>> GMBH{BCD}
+16>> HNCI{CDE}
+17>> IODJ{DEF}
+18>> JPEK{EFG}
+19>> KQFL{FGH}
+20>> LRGM{GHI}
+21>> MSHN{HIJ}
+22>> NTIO{IJK}
+23>> OUJP{JKL}
+24>> PVKQ{KLM}
+25>> QWLR{LMN}
+```
+FLAG{ABC} at 14th line looks like correct flag.
+
+If you are going to solve long caesar-crypto.
+Run solve-all with a word of the passage.
+Then a correct word will be appear.
+Then run solve with the passage, and line number that the word was appear.
--- solver.lisp
+++ solver.lisp
@@ -1,5 +1,3 @@
-(defparameter *ignore* nil)
-
 ; 文字列をASCIIのリストへ。
 (defun str-code (str)
   (mapcar #'char-code
@@ -11,33 +9,40 @@
           asc)
   'string))
 
-; 無視する文字のASCIIは無視して計算
+; 無視する文字のASCIIは無視して計算。
 (defun ascii-shift (lst val)
-  (if (member lst *ignore*)
-    lst
-    (mapcar 
-      (lambda (chr)
-        (setq plused-ascii (+ chr val))
-              (cond ((<= 65 chr 90)
-                     (cond ((< plused-ascii 65) (incf plused-ascii 26)
-                                                plused-ascii)
-                           ((> plused-ascii 90) (decf plused-ascii 26)
-                                                plused-ascii)
-                           (t plused-ascii)))
-                     ((<= 97 chr 122)
-                      (cond ((< plused-ascii 97) (incf plused-ascii 26)
-                                                 plused-ascii)
-                            ((> plused-ascii 122) (decf plused-ascii 26)
-                                                  plused-ascii)
-                            (t plused-ascii)))
-                     (t chr)))
-      lst)))
+  (mapcar 
+    (lambda (chr)
+      (setq plused-ascii (+ chr val))
+      (cond ((<= 65 chr 90)
+             (cond ((< plused-ascii 65) (incf plused-ascii 26)
+                                        plused-ascii)
+                   ((> plused-ascii 90) (decf plused-ascii 26)
+                                        plused-ascii)
+                   (t plused-ascii)))
+            ((<= 97 chr 122)
+             (cond ((< plused-ascii 97) (incf plused-ascii 26)
+                                        plused-ascii)
+                   ((> plused-ascii 122) (decf plused-ascii 26)
+                                         plused-ascii)
+                   (t plused-ascii)))
+            (t chr)))
+    lst))
 
-; 無視する文字を追加
-(defun add-ignore (ignore-str)
-  (mapcar (lambda (x) (pushnew x *ignore*))
-          (mapcar #'char-code (coerce ignore-str 'list)))
-  *ignore*)
-
+; strをval文字ずらす。
 (defun solve (str val)
   (princ (dec-code (ascii-shift (str-code str) val))))
+
+; strを1から26文字ずらした結果を表示
+(defun solve-all (str)
+  (let ((ascii (str-code str)))
+    (loop for i
+          from 1
+          below 26
+          do(progn
+              ;何文字ずらしたか
+              (princ i)
+              (princ ">> ")
+
+              (princ (dec-code (ascii-shift ascii i)))
+              (fresh-line)))))
